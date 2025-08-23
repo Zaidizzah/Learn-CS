ca7092f153347b7b33f6246be5c5b4af21ff30ac
Zaid izzah nurbaain <nailaaqila31@gmail.com>
2025-08-23 17:37:46 +0700

    ADD: Learning how params keyword work in C# methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,8 +15,22 @@ namespace HelloWorld
             //TaskLists taskLists = new();
             //taskLists.Run();
 
-            AuthenticateAPP Authenticate = new();
-            Authenticate.run();
-         }
+            //AuthenticateAPP Authenticate = new();
+            //Authenticate.run();
+
+            //Program.Calculate(10, 15, 15);
+        }
+
+        // Learning how params keyword work in C# methods
+        public static void Calculate(params uint[] Nums)
+        {
+            uint Total = 0;
+            for (ushort Index = 0; Index < Nums.Length; Index++)
+            {
+                Total += Nums[Index];
+            }
+
+            Console.WriteLine($"Total is {Total}");
+        }
     }
 }
