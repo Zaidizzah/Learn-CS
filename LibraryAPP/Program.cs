c7c11c44805c8d74cf08dc99fbd83c08027a3c42
Zaid izzah nurbaain <nailaaqila31@gmail.com>
2025-09-24 14:28:45 +0700

    MODIFY: LibraryApp codes, some logic, and adding some additional function for signed in user

--- LibraryAPP/Program.cs
+++ LibraryAPP/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Reflection;
 
 namespace LibraryApp
 {
@@ -8,29 +10,26 @@ namespace LibraryApp
     {
         static void Main(string[] args)
         {
-            // Tambahkan 1 admin default
+            // Adding 1 default user for Admin role
             Users.AddUser("admin@gmail.com", "Admin123", "Admin");
 
-            bool exit = false;
-            Users currentSession = null;
+            bool Exit = false;
+            Users? CurrentSession = null;
 
-            while (!exit)
+            while (Exit == false)
             {
                 Console.Clear();
-                Console.WriteLine("=== LibraryApp Console ===");
-                Console.WriteLine("1. Sign In");
-                Console.WriteLine("2. Sign Up");
-                Console.WriteLine("0. Keluar");
-                Console.Write("Pilih menu: ");
-                string choice = Console.ReadLine();
+                Console.WriteLine("=== SELAMAT DATANG DI APLIKASI PERPUSTAKAAN CONSOLE ===");
+                Console.Write("Silahkan pilih dari ketiga menu berikut untuk memulai atau berinteraksi dengan aplikasi:\n1. Sign In\n2. Sign Up\n0. Keluar\nPilih menu: ");
+                string? Choice = Console.ReadLine();
 
-                switch (choice)
+                switch (Choice)
                 {
                     case "1":
-                        currentSession = Users.SignIn();
-                        if (currentSession != null)
+                        CurrentSession = Users.SignIn();
+                        if (CurrentSession != null)
                         {
-                            ShowMainMenu(currentSession);
+                            ShowMainMenu(CurrentSession);
                         }
                         break;
 
@@ -39,60 +38,66 @@ namespace LibraryApp
                         break;
 
                     case "0":
-                        exit = true;
+                        Exit = true;
                         break;
 
                     default:
-                        Console.WriteLine("Pilihan tidak valid!");
-                        Console.ReadKey();
+                        Console.WriteLine("Pilihan and tidak valid. Pastikan and memilih pilihan dari 0 (Keluar), 1 (Sign In), atau 2 (Sign Up).");
                         break;
                 }
+
+                Console.WriteLine("\nTekan keypass apapun untuk melanjutkan...");
+                Console.ReadKey();
             }
         }
 
-        static void ShowMainMenu(Users session)
+        static void ShowMainMenu(Users UserSession)
         {
-            bool logout = false;
-            while (!logout)
+            bool IsSignOut = false;
+            while (IsSignOut == false)
             {
                 Console.Clear();
-                Console.WriteLine($"=== Dashboard ({session.Role}) ===");
-                Console.WriteLine("1. Lihat Buku");
-                Console.WriteLine("2. Tambah Buku");
-                Console.WriteLine("3. Cari Buku");
-                if (session.Role == "Admin")
+                Console.WriteLine($"=== DASHBOARD ({UserSession.Role.ToUpper()}) ===");
+                Console.WriteLine("Silahkan memilih menu-menu yang ada untuk berinterkasi dalam applikasi:\n1. Lihat daftar buku yang tersedia\n2. Tambah buku baru\n3. Cari Buku yang ada");
+                if (UserSession.Role == "Admin")
                 {
-                    Console.WriteLine("4. Lihat Semua User");
+                    Console.WriteLine("4. Lihat Semua Pengguna perpustakaan");
                 }
-                Console.WriteLine("9. Logout");
-                Console.Write("Pilih menu: ");
-                string choice = Console.ReadLine();
+                Console.WriteLine("5. Biodata saya\n9. Sign Out/Keluar dari aplikasi\nPilih menu: ");
+                string? Choice = Console.ReadLine();
 
-                switch (choice)
+                switch (Choice)
                 {
                     case "1":
                         Library.ShowBooks();
                         break;
                     case "2":
-                        if (session.Role == "Admin")
+                        if (UserSession.Role == "Admin")
                             Library.AddBook();
                         else
-                            Console.WriteLine("Hanya Admin yang bisa menambahkan buku!");
-                        Console.ReadKey();
+                            Console.WriteLine("Hanya pengguna yang berstatus 'ADMIN' yang bisa menambahkan, menghapus, atau memodifikasi metadata buku!");
                         break;
                     case "3":
                         Library.SearchBook();
                         break;
                     case "4":
-                        if (session.Role == "Admin")
+                        if (UserSession.Role == "Admin")
                             Users.ShowAllUsers();
                         else
-                            Console.WriteLine("Akses ditolak!");
-                        Console.ReadKey();
+                            Console.WriteLine("Akses ditolak, anda mengakses sumber daya yang tidak diperuntukan!");
+                        break;
+                    case "5":
+                        Users.ShowUserAttributes(UserSession.UserId);
                         break;
                     case "9":
-                        logout = true;
+                        IsSignOut = true;
                         break;
+                }
+
+                if (IsSignOut == false)
+                {
+                    Console.WriteLine("\nTekan keypass apapun untuk melanjutkan...");
+                    Console.ReadKey();
                 }
             }
         }
@@ -109,44 +114,52 @@ namespace LibraryApp
 
     class Users
     {
-        private static Dictionary<string, User> _users = new();
+        private static Dictionary<string, User> _Users = new();
 
         public string UserId { get; private set; }
         public string Role { get; private set; }
 
-        private Users(User user)
+        private Users(User User)
         {
-            UserId = user.Id;
-            Role = user.Role;
+            UserId = User.Id;
+            Role = User.Role;
         }
 
-        public static void AddUser(string email, string password, string role)
-        {
-            if (_users.Values.Any(u => u.Email == email)) return;
-            string id = $"ID-{(_users.Count + 1).ToString("D2")}";
-            _users[id] = new User { Id = id, Email = email, Password = password, Role = role };
+        public static User? GetUserAttributes(string UserId) {
+            if (_Users.TryGetValue(UserId, out User User))
+            {
+                return User;
+            } else
+            {
+                return null;
+            }
         }
 
-        public static Users SignIn()
+        public static void AddUser(string Email, string Password, string role)
+        {
+            if (_Users.Values.Any(u => u.Email == Email)) return;
+            string id = $"ID-{(_Users.Count + 1).ToString("D2")}";
+            _Users[id] = new User { Id = id, Email = Email, Password = Password, Role = role };
+        }
+
+        public static Users? SignIn()
         {
             Console.Clear();
-            Console.WriteLine("=== Sign In ===");
-            Console.Write("Email: ");
-            string email = Console.ReadLine();
-            Console.Write("Password: ");
-            string password = Console.ReadLine();
+            Console.WriteLine("=== HALAMAN SIGN IN ===");
+            Console.Write("Masukkan Email: ");
+            string? Email = Console.ReadLine();
+            Console.Write("Masukkan Password: ");
+            string? Password = Console.ReadLine();
 
-            var user = _users.Values.FirstOrDefault(u => u.Email == email && u.Password == password);
-            if (user != null)
+            User? User = _Users.Values.FirstOrDefault(u => u.Email == Email && u.Password == Password);
+            if (User != null)
             {
-                Console.WriteLine($"Selamat datang, {user.Email}!");
-                Console.ReadKey();
-                return new Users(user);
+                Console.WriteLine($"Selamat datang diaplikasi perpustakaan, {User.Email}. Silahkan menggunakan aplikasi secara bijak.");
+                return new Users(User);
             }
             else
             {
-                Console.WriteLine("Login gagal.");
-                Console.ReadKey();
+                Console.WriteLine("Prosess Sign In/Masuk gagal. Harap coba lagi dan periksa nilai Email dan Password anda.");
                 return null;
             }
         }
@@ -154,33 +167,50 @@ namespace LibraryApp
         public static void SignUp()
         {
             Console.Clear();
-            Console.WriteLine("=== Sign Up ===");
-            Console.Write("Email: ");
-            string email = Console.ReadLine();
-            Console.Write("Password: ");
-            string password = Console.ReadLine();
+            Console.WriteLine("=== HALAMAN SIGN UP ===");
+            Console.Write("Masukkan Email: ");
+            string? Email = Console.ReadLine();
+            Console.Write("Masukkan Password: ");
+            string? Password = Console.ReadLine();
 
-            if (_users.Values.Any(u => u.Email == email))
+            if (_Users.Values.Any(u => u.Email == Email))
             {
-                Console.WriteLine("Email sudah terdaftar!");
+                Console.WriteLine("Email telah terdaftar sebelumnya diaplikasi perpustakaan! silahkan gunakan email lain");
             }
             else
             {
-                AddUser(email, password, "User");
-                Console.WriteLine("Registrasi berhasil!");
+                AddUser(Email, Password, "User");
+                Console.WriteLine("Prosess Registrasi berhasil silahkan masuk dengan akun pengguna baru anda!");
             }
-            Console.ReadKey();
         }
 
         public static void ShowAllUsers()
         {
             Console.Clear();
-            Console.WriteLine("=== Daftar Pengguna ===");
-            foreach (var u in _users.Values)
+            Console.WriteLine("=== DAFTAR PENGGUNA APLIKASI PERPUSTAKAAN ===");
+            foreach (var u in _Users.Values)
             {
                 Console.WriteLine($"{u.Id} - {u.Email} ({u.Role})");
             }
-            Console.ReadKey();
+        }
+
+        public static void ShowUserAttributes(string UserId)
+        {
+            Console.Clear();
+            if (_Users.TryGetValue(UserId, out User User))
+            {
+                Console.WriteLine("=== BIODATA ANDA ===");
+                List<PropertyInfo> UserAttributes = typeof(User).GetProperties().OrderByDescending(p => p.Name.Length).ToList();
+                int LongestPropertyName = UserAttributes.Max(p => p.Name.Length);
+                string PropertyName;
+
+                foreach (PropertyInfo p in UserAttributes)
+                {
+                    if (p.Name == "Id") continue;
+                    PropertyName = p.Name.PadRight(LongestPropertyName, ' ');
+                    Console.WriteLine($"{PropertyName} = {p.GetValue(User)}");
+                }
+            }
         }
     }
 
@@ -194,51 +224,55 @@ namespace LibraryApp
 
     class Library
     {
-        private static List<Book> _books = new();
+        private static List<Book> _Books = new();
 
         public static void AddBook()
         {
             Console.Clear();
-            Console.WriteLine("=== Tambah Buku ===");
-            Console.Write("Judul: ");
-            string title = Console.ReadLine();
-            Console.Write("Pengarang: ");
-            string author = Console.ReadLine();
-            Console.Write("Penerbit: ");
-            string publisher = Console.ReadLine();
-            Console.Write("ISBN: ");
-            string isbn = Console.ReadLine();
+            Console.WriteLine("=== TAMBAH BUKU BARU ===");
+            Console.Write("Masukkan Judul Buku: ");
+            string? Title = Console.ReadLine();
+            Console.Write("Masukkan Pengarang Buku: ");
+            string? Author = Console.ReadLine();
+            Console.Write("Masukkan Penerbit Buku: ");
+            string? Publisher = Console.ReadLine();
+            Console.Write("Masukkan ISBN Buku: ");
+            string? ISBN = Console.ReadLine();
 
-            _books.Add(new Book { Title = title, Author = author, Publisher = publisher, ISBN = isbn });
-            Console.WriteLine("Buku berhasil ditambahkan!");
+            if (Title == null || Author == null || Publisher == null || ISBN == null)
+            {
+                Console.WriteLine("Salah satu metdata buku yang anda masukkan tidak valid ataur bernilai kosong [NULL]. Mohon coba lagi.");
+                return;
+            }
+
+            _Books.Add(new Book { Title = Title, Author = Author, Publisher = Publisher, ISBN = ISBN });
+            Console.WriteLine($"Buku dengan judul '{Title}' berhasil ditambahkan kedaftar buku perpustakaan!");
         }
 
         public static void ShowBooks()
         {
             Console.Clear();
-            Console.WriteLine("=== Daftar Buku ===");
-            if (_books.Count == 0) Console.WriteLine("Belum ada buku.");
-            foreach (var b in _books)
+            Console.WriteLine("=== DAFTAR BUKU PERPUSTAKAAN ===");
+            if (_Books.Count == 0) Console.WriteLine("Belum ada buku yang tersimpan dalam perpustakaan.");
+            foreach (var b in _Books)
             {
                 Console.WriteLine($"{b.Title} - {b.Author} ({b.Publisher}) [{b.ISBN}]");
             }
-            Console.ReadKey();
         }
 
         public static void SearchBook()
         {
             Console.Clear();
-            Console.Write("Masukkan kata kunci judul: ");
-            string keyword = Console.ReadLine();
-            var results = _books.Where(b => b.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+            Console.Write("Masukkan kata kunci judul yang ingin anda cari: ");
+            string? Keyword = Console.ReadLine();
+            List<Book> results = _Books.Where(b => b.Title.Contains(Keyword, StringComparison.OrdinalIgnoreCase)).ToList();
 
-            Console.WriteLine("=== Hasil Pencarian ===");
-            if (results.Count == 0) Console.WriteLine("Tidak ditemukan.");
-            foreach (var b in results)
+            Console.WriteLine($"=== HASIL PENCARIAN: '{Keyword}' ===");
+            if (results.Count == 0) Console.WriteLine($"Tidak ditemukan judul buku terkait pencarian: '{Keyword}'.");
+            foreach (Book b in results)
             {
                 Console.WriteLine($"{b.Title} - {b.Author} ({b.Publisher}) [{b.ISBN}]");
             }
-            Console.ReadKey();
         }
     }
 }
